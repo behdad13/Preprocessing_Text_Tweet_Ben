c3b04be9dcbaa8fffb657611bf96a0d8e9ebde33
Behdad Ehsani <58978680+behdad13@users.noreply.github.com>
2023-02-19 12:18:58 -0500

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Preprocessing python package
+
+#### for more information, PLZ send an email: behdad.ehsani@hec.ca
+
+
+This package is created by Behdad Ehsani.
+
+
---
+++ preprocessing_text_ben/__init__.py
@@ -0,0 +1,73 @@
+from preprocessing_text_ben import utils
+
+__version__ = '0.0.1'
+
+def word_counts(x):
+	return utils._get_word_counts(x)
+
+def char_counts(x):
+	return utils._get_char_counts(x)
+
+def avg_wordlength(x):
+	return utils._get_avg_wordlength(x)
+
+def stopwords_count(x):
+	return utils._get_stopwords_count(x)
+
+def hashtag_couts(x):
+	return utils._get_hashtag_couts(x)
+
+def mention_couts(x):
+	return utils._get_mention_couts(x)
+
+def digit_counts(x):
+	return utils._get_digit_counts(x)
+
+def uppercase_count(x):
+	return utils._get_uppercase_count(x)
+
+def cont_to_exp(x):
+	return utils._cont_to_exp(x)
+
+def emails(x):
+	return utils._get_emails(x)
+
+def remove_emails(x):
+	return utils._remove_emails(x)
+
+def urls(x):
+	return utils._get_urls(x)
+
+def remove_urls(x):
+	return utils._remove_urls(x)
+
+def remove_rt(x):
+	return utils._remove_rt(x)
+
+def remove_special_chars(x):
+	return utils._remove_special_chars(x)
+
+def remove_html_tags(x):
+	return utils._remove_html_tags(x)
+
+def remove_accented_chars(x):
+	return utils._remove_accented_chars(x)
+
+def remove_stopwords(x):
+	return utils._remove_stopwords(x)
+
+def make_base(x):
+	return utils._make_base(x)
+
+def remove_common_words(x, n=20):
+	return utils._remove_common_words(x)
+
+def remove_rare_words(x, n=20):
+	return utils._remove_rare_words(x)
+
+def spelling_checking(x):
+	return utils._spelling_checking(x)
+
+
+
+
